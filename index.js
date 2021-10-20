49f7fbf33d18b9139b5374c051f9667dcc0b86a4
Nicolas Payrouse <wawawoom@gmail.com>
2021-10-20 22:43:04 +0200

    --

--- index.js
+++ index.js
@@ -1,64 +1,54 @@
-const styles: any = {
-  log: "background: #FFCC00; color: #000000;",
-  info: "background: #7cb5e5; color: #000000;",
-  success: "background: #BADA55; color: #222222;",
-  error: "background: #FF0000; color: #FFFFFF;",
-  title:
-    "font-size: 0.8rem; font-weight: lighter; background: #3E3E3E; color: #FFFFFF; padding: 3px 6px;",
+"use strict";
+exports.__esModule = true;
+var styles = {
+    log: "background: #FFCC00; color: #000000;",
+    info: "background: #7cb5e5; color: #000000;",
+    success: "background: #BADA55; color: #222222;",
+    error: "background: #FF0000; color: #FFFFFF;",
+    title: "font-size: 0.8rem; font-weight: lighter; background: #3E3E3E; color: #FFFFFF; padding: 3px 6px;"
 };
-
 // const Konsole = {
 //   log: ({ url, method, data, response }) => {
 //     if (typeof url !== "undefined") {
 //       console.log("%c url      => ", labelStyleInfo, url);
 //     }
-
 //     if (typeof method !== "undefined") {
 //       console.log("%c method   => ", labelStyleInfo, method);
 //     }
-
 //     if (typeof data !== "undefined") {
 //       console.log("%c data     => ", labelStyleInfo, data);
 //     }
-
 //     if (typeof response !== "undefined") {
 //       console.log("%c response => ", labelStyleSuccess, response);
 //     }
 //   },
-
 //   error: ({ url, method, response }) => {
 //     if (typeof url !== "undefined") {
 //       console.log("%c url      => ", labelStyleInfo, url);
 //     }
-
 //     if (typeof method !== "undefined") {
 //       console.log("%c method   => ", labelStyleInfo, method);
 //     }
-
 //     if (typeof response !== "undefined") {
 //       console.log("%c response => ", labelStyleError, response);
 //     }
 //   },
-
 //   group: (functionName) => {
 //     console.group("%c 👉 " + functionName, labelTitle);
 //   },
-
 //   endGroup: () => {
 //     console.groupEnd();
 //   },
 // };
-
-const Konsole = {
-  log: (payload: any, name?: string) => {
-    console.log(`%c ${name ? name : ""} => ${payload}`, styles.log);
-  },
-  group: (name: string) => {
-    console.group("%c 👉 " + name, styles.title);
-  },
-  endGroup: () => {
-    console.groupEnd();
-  },
+var Konsole = {
+    log: function (payload, name) {
+        console.log("%c " + name + " => ", payload);
+    },
+    group: function (name) {
+        console.group("%c 👉 " + name, styles.title);
+    },
+    endGroup: function () {
+        console.groupEnd();
+    }
 };
-
-export default Konsole;
+exports["default"] = Konsole;
